0e58ee1507ecf29237ef12cf60d31b67198760e8
renqabs <130155002+renqabs@users.noreply.github.com>
2023-11-24 09:16:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
-nohup ./PandoraNext &
-function update_license {
-    curl -fLO https://dash.pandoranext.com/data/$(cat /etc/secrets/LICENSE_URL)/license.jwt
-    curl -H "Authorization: Bearer $(cat /etc/secrets/SETUP_PASSWORD)" -X POST "http://localhost:8080/setup/reload"
-}
-# 无限循环
-while true
-do
-    sleep 600       # 暂停600秒（10分钟）
-    update_license   # 调用函数
-done 
+curl -fLO https://dash.pandoranext.com/data/$(cat /etc/secrets/LICENSE_URL)/license.jwt
+./PandoraNext
